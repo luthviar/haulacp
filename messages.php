6d46b8a792683d934badc2cf85b25fab005294a2
Luthfi Abdurrahim <luthviar.a@gmail.com>
2018-07-21 22:19:58 +0700

    fix form

--- messages.php
+++ messages.php
@@ -7,10 +7,11 @@
             $name       = $_POST['name'];
             $email      = $_POST['email'];
             $asal       = $_POST['asal'];
+            $nohp       = $_POST['nohp'];
             $subject    = $_POST['subject'];
             $message    = $_POST['message'];
 
-            $query = "INSERT INTO `messages`(`nama`, `email`, `judul`, `instansi`, `pesan`) VALUES ('".$name."','".$email."','".$subject."','".$asal."','".$message."')";
+            $query = "INSERT INTO `messages`(`nama`, `email`, `judul`, `instansi`,`no_hp`, `pesan`) VALUES ('".$name."','".$email."','".$subject."','".$asal."','".$nohp."','".$message."')";
             $result = mysqli_query($conn,$query);
 
             if($result){
